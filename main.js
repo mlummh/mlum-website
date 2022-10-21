db9f6f9a3104563d80c9566ef89d862e870b2874
kiip1 <25848425+kiip1@users.noreply.github.com>
2022-10-21 14:39:16 +0200

    Update site

--- main.js
+++ main.js
@@ -2,16 +2,6 @@ const data = async () => (await (await fetch('https://api.minehut.com/server/5a4
 
 (loop = async () => {
   let currentData = await data();
-
   document.getElementById('playerCount').innerText = currentData.playerCount;
-
-  document.getElementById('players').innerHTML = '';
-  for (const player of currentData.players) {
-    document.getElementById('players').insertAdjacentHTML(
-      'beforeend',
-      `<img src="https://minotar.net/helm/${player.trim()}" alt="${player.trim()}" width="32" height="32">`
-    );
-  }
-
   setTimeout(loop, 10000);
 })();
